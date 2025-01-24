27dfacdda81567175b379d5664f443c7d782dacd
Ben Booker <ben.o.booker@gmail.com>
2025-01-25 09:59:34 +1100

    object orientification and added constants.py

---
+++ constants.py
@@ -0,0 +1,6 @@
+from game_interface import ClickField
+
+RUNELITE_LAUNCH_TIME = 12
+
+# Login Fields
+EXISTING_USER = ClickField(395, 535, 300, 338)
--- game_interface.py
+++ game_interface.py
@@ -6,50 +6,72 @@ from tqdm import tqdm
 import pyautogui as pag
 import random
 
-rlLaunchTime = 8
+from constants import *
 
-def natural_click(min_x, max_x, min_y, max_y):
-    pag.click(random.randint(min_x, max_x), random.randint(min_y, max_y))
+class ClickField():
+    def __init__(self, min_x: int, max_x: int, min_y: int, max_y: int):
+        self.min_x = min_x
+        self.max_x = max_x
+        self.min_y = min_y
+        self.max_y = max_y
+
+    def __repr__(self):
+            return f"ClickField(min_x={self.min_x}, max_x={self.max_x}, min_y={self.min_y}, max_y={self.max_y})"
+    
+    def natural_click(self):
+        """Performs a click at a random location within the defined field."""
+        x = random.randint(self.min_x, self.max_x)
+        y = random.randint(self.min_y, self.max_y)
+        pag.click(x, y)
+
 
 def natural_write(text):
     for char in text:
         pag.write(char)
         sleep(random.uniform(0.05, 1.5))
 
-def initialise(username, password):
 
-    # if RuneLite application is not running, launch it quietly
-    if len(pwc.getWindowsWithTitle("RuneLite")) == 0:
-        print("Opening Runelite...")
-        subprocess.Popen(
-            ["runelite"],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL
-        )
-        # wait for RuneLite GUI to load
-        for i in tqdm(range(100)):
-            sleep(rlLaunchTime/100)
-    else:
-        print("RuneLite is already running.")
-    
-    # position and resize RuneLite window
-    runeliteInstances = pwc.getWindowsWithTitle('RuneLite')
-    rl1 = runeliteInstances[0]
-    rl1.resizeTo(750,500)
-    rl1.moveTo(0,0)
-    
-    # Log into account
-    print(f"Logging into account {username}")
-    natural_click(395, 535, 300, 338)
-    natural_write(username)
-    pag.press('tab')
-    natural_write(password)
-    pag.press('enter')
+class RLInstance():
+    def __init__(self, username: str, password: str, membership: bool):
+        self.username = username
+        self.password = password
+        self.membership = membership
+
+    def setup(self):
+        # if RuneLite application is not running, launch it quietly
+        if len(pwc.getWindowsWithTitle("RuneLite")) == 0:
+            print("Opening Runelite...")
+            subprocess.Popen(
+                ["runelite"],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL
+            )
+            # wait for RuneLite GUI to load
+            for i in tqdm(range(100)):
+                sleep(RUNELITE_LAUNCH_TIME/100)
+        else:
+            print("RuneLite is already running.")
+        
+        # position and resize RuneLite window
+        runelite_instances = pwc.getWindowsWithTitle('RuneLite')
+        instance = runelite_instances[0]
+        instance.raiseWindow()
+        instance.resizeTo(750,500)
+        instance.moveTo(0,0)
+        
+        # Log into account
+        print(f"Logging into account {self.username}")
+        EXISTING_USER.natural_click()
+        natural_write(self.username)
+        pag.press('tab')
+        natural_write(self.password)
+        pag.press('enter')
+            
         
 if __name__ == "__main__":
-    initialise("userrnaaamee", "passworrrddd")
+    username = "yo"
+    password = "yo"
+    membership = False
 
-
-
-
-
+    rl_instance = RLInstance(username, password, membership)
+    rl_instance.setup()
